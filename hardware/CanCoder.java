987a95935d7fc33ae60953bd8d62e37311b129b0
misaka10987 <misaka10987@outlook.com>
2024-11-01 12:00:46 +0800

    use lazy initialization

--- hardware/CanCoder.java
+++ hardware/CanCoder.java
@@ -2,13 +2,15 @@ package frc.libzodiac.hardware;
 
 import com.ctre.phoenix6.hardware.CANcoder;
 
+import frc.libzodiac.util.Lazy;
+
 public class CanCoder {
     public static final double POSITION_RAW_UNIT = 2 * Math.PI;
-    private final CANcoder encoder;
+    private final Lazy<CANcoder> encoder;
     public double zero = 0;
 
     public CanCoder(int can_id) {
-        this.encoder = new CANcoder(can_id);
+        this.encoder = new Lazy<>(() -> new CANcoder(can_id));
     }
 
     public CanCoder set_zero(double zero) {
@@ -17,6 +19,6 @@ public class CanCoder {
     }
 
     public double get() {
-        return (this.encoder.getAbsolutePosition().refresh().getValue() - this.zero) * POSITION_RAW_UNIT;
+        return (this.encoder.get().getAbsolutePosition().refresh().getValue() - this.zero) * POSITION_RAW_UNIT;
     }
 }
