67e5f52103a89a05f5d548171d2517330a1f13f5
misaka10987 <misaka10987@outlook.com>
2024-11-25 18:18:31 +0800

    deprecate `Zervo` due to api change of `ZMotor`

--- Zervo.java
+++ Zervo.java
@@ -3,6 +3,7 @@ package frc.libzodiac;
 /**
  * Note: the method Zmotor.go **MUST** be overridden.
  */
+@Deprecated
 public interface Zervo {
     /**
      * Configures the zero position of the motor.
