8856db831cb9c37dd8675d16f73dfc63632d0dc4
Blue-Flag-666 <64064866+Blue-Flag-666@users.noreply.github.com>
2025-01-28 11:33:02 +0800

    Add command to rumble controller

--- util/CommandUtil.java
+++ util/CommandUtil.java
@@ -5,7 +5,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class CommandUtil {
-    public static Command rumbleController(GenericHID controller, double intensity, double duration) {
+    public static void rumbleController(GenericHID controller, double intensity, double duration) {
+        rumbleControllerCommand(controller, intensity, duration).schedule();
+    }
+
+    public static Command rumbleControllerCommand(GenericHID controller, double intensity, double duration) {
         return Commands.runOnce(() -> controller.setRumble(GenericHID.RumbleType.kBothRumble, intensity)).repeatedly()
                 .withTimeout(duration).finallyDo(() -> controller.setRumble(GenericHID.RumbleType.kBothRumble, 0));
     }
